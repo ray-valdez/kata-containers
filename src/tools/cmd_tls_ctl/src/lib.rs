26346df055672c1447e0ae0f25f810d6f70a2773
Ray Valdez <rvaldez@us.ibm.com>
2022-08-18 15:23:37 -0400

    cmd: add resume

--- src/tools/cmd_tls_ctl/src/lib.rs
+++ src/tools/cmd_tls_ctl/src/lib.rs
@@ -1,11 +1,31 @@
+pub enum PauseKind {
+    PAUSE,
+    RESUME,
+}
+
 pub struct Config {
+    pub cmd: PauseKind,
     pub address: String,
-    pub cid: String
+    pub cid: String,
 }
 
 impl Config {
-    pub fn new (mut args: std::env::Args) -> Result<Config, &'static str> {
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
         args.next(); // skip name of program
+
+        let cmd: PauseKind = match args.next() {
+            Some(arg) => {
+                if arg.eq("pause") {
+                    PauseKind::PAUSE
+                } else if arg.eq("resume") {
+                    PauseKind::RESUME
+                } else {
+                    return Err("Incorrect command: [pause | resume]");
+                }
+            }
+            None => return Err("Missing: [pause | resume]"),
+        };
+
         let address = match args.next() {
             Some(arg) => arg,
             None => return Err("Missing: [kata_agent IP address]"),
@@ -16,11 +36,6 @@ impl Config {
             None => return Err("Missing: [container id]"),
         };
 
-        Ok(Config { address, cid })
+        Ok(Config { cmd, address, cid })
     }
 }
-
-
-
-
-
--- src/tools/cmd_tls_ctl/src/pause_cmd.rs
+++ src/tools/cmd_tls_ctl/src/pause_cmd.rs
@@ -1,4 +1,4 @@
-use cmd_tls_ctl::Config;
+use cmd_tls_ctl::{Config, PauseKind};
 use std::env;
 use std::process;
 
@@ -8,11 +8,10 @@ pub mod grpctls {
 }
 
 use grpctls::sec_agent_service_client::SecAgentServiceClient;
-use grpctls::SecPauseContainerRequest;
+use grpctls::{PauseContainerRequest, ResumeContainerRequest};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-
     let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
@@ -23,28 +22,27 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let str_front = "http://";
     let str_end = ":50051";
 
-    let url_string = format!("{}{}{}",str_front, config.address, str_end);
+    let url_string = format!("{}{}{}", str_front, config.address, str_end);
     println!("url_string {}", url_string);
 
-
     // Get certificate from disk
-    let cert=include_str!("../keys/client.pem");
-    let key=include_str!("../keys/client.key");
+    let cert = include_str!("../keys/client.pem");
+    let key = include_str!("../keys/client.key");
 
     // Create identify from key and certificate
-    let id=tonic::transport::Identity::from_pem(cert.as_bytes(),key.as_bytes());
+    let id = tonic::transport::Identity::from_pem(cert.as_bytes(), key.as_bytes());
 
     // Import our certificate for CA
-    let s=include_str!("../keys/my_ca.pem");
+    let s = include_str!("../keys/my_ca.pem");
 
     // Convert it into a certificate
-    let ca=tonic::transport::Certificate::from_pem(s.as_bytes());
+    let ca = tonic::transport::Certificate::from_pem(s.as_bytes());
 
     // Telling our client what is the identity of our server
-    let tls=tonic::transport::ClientTlsConfig::new().
-       domain_name("localhost").
-       identity(id).
-       ca_certificate(ca);
+    let tls = tonic::transport::ClientTlsConfig::new()
+        .domain_name("localhost")
+        .identity(id)
+        .ca_certificate(ca);
 
     let channel = tonic::transport::Channel::from_shared(url_string.to_string()).unwrap();
     let channel = channel.tls_config(tls)?.connect().await?;
@@ -52,13 +50,32 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Create gRPC client from channel
     let mut client = SecAgentServiceClient::new(channel);
 
+    match config.cmd {
+        PauseKind::PAUSE => {
+            let request = tonic::Request::new(PauseContainerRequest {
+                container_id: config.cid,
+            });
+            let response = client.pause_container(request).await?.into_inner();
+            println!("RESPONSE={:?}", response);
+        }
+
+        PauseKind::RESUME => {
+            let request = tonic::Request::new(ResumeContainerRequest {
+                container_id: config.cid,
+            });
+            let response = client.resume_container(request).await?.into_inner();
+            println!("RESPONSE={:?}", response);
+        }
+    }
     // Create a new Request
-    let request = tonic::Request::new(SecPauseContainerRequest {
+    /*
+    let request = tonic::Request::new(PauseContainerRequest {
         container_id: config.cid,
     });
 
     // Send request and waiting for response
-    let response = client.sec_pause_container(request).await?.into_inner();
+    let response = client.pause_container(request).await?.into_inner();
     println!("RESPONSE={:?}", response);
+    */
     Ok(())
 }
